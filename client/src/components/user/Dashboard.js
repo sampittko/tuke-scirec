8a2e5a707a4b219c65ed1d444afe22e91f4c92ee
Samuel Pitoňák <samuel.pitonak@student.tuke.sk>
2019-02-11 16:15:08 +0100

    variables and spacing unit added

--- client/src/components/user/Dashboard.js
+++ client/src/components/user/Dashboard.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Fab, Typography, Paper } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import './Dashboard.scss';
 
 class Dashboard extends React.Component {
     render() {
         return (
-            "Dashboard.js"
+            <Paper className="dashboard">
+                <Typography variant="h5">
+                    Projects
+                </Typography>
+                <Fab className="fab" color="secondary">
+                    <AddIcon />
+                </Fab>
+            </Paper>
         );
     }
 }
