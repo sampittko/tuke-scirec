75f5dcfa44aebef963f0478198fab893c62dac0c
Samuel Pitoňák <samuel.pitonak@student.tuke.sk>
2019-03-21 20:29:11 +0100

    - PhpStorm code cleanup and rearrangement - add TransitionComponent prop for dialogs as DialogTransition

---
+++ client/src/components/common/DialogTransition.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Slide } from '@material-ui/core';
+
+const DialogTransition = props =>
+  <Slide direction="up" {...props} />;
+
+export default DialogTransition;
